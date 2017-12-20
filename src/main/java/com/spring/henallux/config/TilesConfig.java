1df7646908df9ea402831760aa792e4ac19afae4
Unknown <jasgenin06@gmail.com>
2017-12-20 21:19:18 +0100

    indentation CTRL ALT L

--- src/main/java/com/spring/henallux/config/TilesConfig.java
+++ src/main/java/com/spring/henallux/config/TilesConfig.java
@@ -9,23 +9,21 @@ import org.springframework.web.servlet.view.tiles3.TilesView;
 
 @Configuration
 public class TilesConfig {
-	
-	@Bean
-	public TilesConfigurer tilesConfigurer()
-	{
-		final TilesConfigurer configurer = new TilesConfigurer();
-		configurer.setDefinitions(new String[] {"WEB-INF/resources/tiles.xml"});
-		configurer.setCheckRefresh(true);
-		return configurer;
-	}
-	
-	@Bean
-	public ViewResolver tilesViewResolver ()
-	{
-		final TilesViewResolver resolver = new TilesViewResolver();
-		resolver.setViewClass(TilesView.class);
-		resolver.setExposeContextBeansAsAttributes(true);
-		return resolver;
-	}
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        final TilesConfigurer configurer = new TilesConfigurer();
+        configurer.setDefinitions(new String[]{"WEB-INF/resources/tiles.xml"});
+        configurer.setCheckRefresh(true);
+        return configurer;
+    }
+
+    @Bean
+    public ViewResolver tilesViewResolver() {
+        final TilesViewResolver resolver = new TilesViewResolver();
+        resolver.setViewClass(TilesView.class);
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
 
 }
